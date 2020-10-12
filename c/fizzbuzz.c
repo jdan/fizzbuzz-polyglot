80073e1c7ed4c653dd04a3320de08ec0fa5210b0
Roman Gusev <rgusev2@gmail.com>
2020-10-12 22:35:36 +0600

    Add C implementation

---
+++ c/fizzbuzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 1;
+
+  for (; i <= 100; ++i)
+  {
+    if (i % 15 == 0)
+    {
+      printf("FizzBuzz\n");
+    }
+    else if (i % 3 == 0)
+    {
+      printf("Fizz\n");
+    }
+    else if (i % 5 == 0)
+    {
+      printf("Buzz\n");
+    }
+    else
+    {
+      printf("%i\n", i);
+    }
+  }
+
+  return 0;
+}
